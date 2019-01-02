beaa6d902e1e7f777e320f2ebe463f97f0f97bb4
thangthin <thangthin@gmail.com>
2019-01-02 17:07:38 -0600

    tagging name for instance

--- deploy-single-server/main.tf
+++ deploy-single-server/main.tf
@@ -4,7 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "example" {
     ami = "ami-009d6802948d06e52",
-    instance_type = "t2.micro"
+    instance_type = "t2.micro",
+    tags = {
+        Name = "terraform-example"
+    }
 }
 
 # general syntax for terraform resource
